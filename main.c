bf824ffbecf23f0f56960f63bc2281dcc559f9a9
Gynar <hojin6865@gmail.com>
2017-11-25 15:21:05 +0900

    main code rewritten

--- main.c
+++ main.c
@@ -24,6 +24,9 @@ typedef struct tag_argSock_t{
 	comtype_t m_cmd;
 }argSock_t;
 
+// thread global bar
+
+// function for thead
 void* c_thread(void* arg){
 	int n, rc, fd;
 	comtype_t cmd;
@@ -31,6 +34,8 @@ void* c_thread(void* arg){
 	struct sockaddr_in cliaddr;
 
 	uint8_t buf[4096];
+	uint8_t scratch_raw[4096];
+	coap_rw_buffer_t scratch_buf = {scratch_raw, sizeof(scratch_raw)};
 	socklen_t len;
 	coap_packet_t pkt;
 	
@@ -47,7 +52,7 @@ void* c_thread(void* arg){
 		size_t rsplen = sizeof(buf);
 		coap_packet_t rsppkt;
 
-		coap_handle_req(&scratch_buf, &pkt &rsppkt);
+		coap_handle_req(&scratch_buf, &pkt, &rsppkt);
 
 		rc = coap_build(buf, &rsplen, &rsppkt);
 		if(!rc)
@@ -58,28 +63,29 @@ void* c_thread(void* arg){
 	}
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
+	// socket var
 	int fd;
-	struct sockaddr_in servaddr, cliaddr;
-	uint8_t buf[4096];
-	uint8_t scratch_raw[4096];
-	coap_rw_buffer_t scratch_buf = {scratch_raw, sizeof(scratch_raw)};
-	// create socket
+	struct sockaddr_in servaddr;
+	// thread var
+		
+
+
+
+
+	// socket establishing
+		// create socket
 	fd = socket(AF_INET,SOCK_DGRAM,0);
-	
 	bzero(&servaddr,sizeof(servaddr));
-	
-	// socket config
+		// socket config
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port = htons(PORT);
-	
-	// socket binding
+		// socket binding
 	bind(fd,(struct sockaddr *)&servaddr, sizeof(servaddr));
-	
+		// endpoint setup
 	endpoint_setup();
-	
+		
 	// my program loop
 	for(;;){
 		
@@ -96,46 +102,4 @@ int main(int argc, char **argv)
 		while(1){
 		}
 	}
-
-	while(1)
-	{
-		int n, rc;
-		socklen_t len = sizeof(cliaddr);
-		coap_packet_t pkt;
-
-		n = recvfrom(fd, buf, sizeof(buf), 0, (struct sockaddr *)&cliaddr, &len);
-	#ifdef DEBUG
-		printf("Received: ");
-		coap_dump(buf, n, true);
-		printf("\n");
-	#endif
-
-		if (0 != (rc = coap_parse(&pkt, buf, n)))
-			printf("Bad packet rc=%d\n", rc);
-	        else
-        	{
-			size_t rsplen = sizeof(buf);
-			coap_packet_t rsppkt;
-#ifdef DEBUG
-            coap_dumpPacket(&pkt);
-#endif
-            coap_handle_req(&scratch_buf, &pkt, &rsppkt);
-
-            if (0 != (rc = coap_build(buf, &rsplen, &rsppkt)))
-                printf("coap_build failed rc=%d\n", rc);
-            else
-            {
-#ifdef DEBUG
-                printf("Sending: ");
-                coap_dump(buf, rsplen, true);
-                printf("\n");
-#endif
-#ifdef DEBUG
-                coap_dumpPacket(&rsppkt);
-#endif
-
-                sendto(fd, buf, rsplen, 0, (struct sockaddr *)&cliaddr, sizeof(cliaddr));
-            }
-        }
-    }
 }
